7fd748dbf2ebc3d9e1f4073a4df8a527a60f187f
Beth Marshall <beth.ellieday@gmail.com>
2022-09-01 15:36:30 -0400

    updated form with validation.

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -4,8 +4,10 @@ import org.launchcode.codingevents.data.EventsData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -16,25 +18,34 @@ public class EventController {
 
     @GetMapping
     public String displayAllEvents(Model model){
+        model.addAttribute("title", "All Events");
         model.addAttribute("events", EventsData.getAll());
         return "events/index";
     }
 
     //lives at /events/create
     @GetMapping("create")
-    public String renderCreateEvent(){
+    public String renderCreateEvent(Model model){
+        model.addAttribute("title", "Create Event");
+        model.addAttribute("event", new Event());
         return "events/create";
     }
 
     //lives at /events/create
     @PostMapping("create")
-    public String createEvent(@ModelAttribute Event newEvent){
+    public String createEvent(@ModelAttribute @Valid Event newEvent, Errors errors, Model model){
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Create Event");
+            model.addAttribute("errorMsg", "Bad data!");
+            return "events/create";
+        }
         EventsData.add(newEvent);
         return "redirect:";
     }
 
     @GetMapping("delete")
     public String deleteEventForm(Model model){
+        model.addAttribute("delete", "Delete Events");
         model.addAttribute("events", EventsData.getAll());
         return "events/delete";
     }
--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,20 +1,33 @@
 package org.launchcode.codingevents.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 public class Event {
 
+    @NotBlank
+    @Size(min = 3, max = 20, message = "Name must be between 3 and 20 characters.")
     private String name;
+
+    @Size(max=500, message = "Description too long.")
     private String description;
+
+    @NotBlank
+    @Email(message = "Invalid email. Please try again")
+    private String contactEmail;
     private int id;
     private static int nextId = 1;
 
-    public Event(String name, String description) {
+    public Event(String name, String description, String contactEmail) {
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
         this.id = nextId;
         nextId++;
     }
+    public Event(){}
 
     public String getDescription() {
         return description;
@@ -34,6 +47,14 @@ public class Event {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
     }
 
     @Override
