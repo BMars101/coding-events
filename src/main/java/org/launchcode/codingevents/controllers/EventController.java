379820cc938d9fb53225561f6a0aeaf7d11b1b55
Beth Marshall <beth.ellieday@gmail.com>
2022-09-01 08:31:55 -0400

    pushing changes for the edit event exercises

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -16,13 +16,15 @@ public class EventController {
 
     @GetMapping
     public String displayAllEvents(Model model){
+        model.addAttribute("title", "All Events");
         model.addAttribute("events", EventsData.getAll());
         return "events/index";
     }
 
     //lives at /events/create
     @GetMapping("create")
-    public String renderCreateEvent(){
+    public String renderCreateEvent(Model model){
+        model.addAttribute("title", "Create Event");
         return "events/create";
     }
 
@@ -35,6 +37,7 @@ public class EventController {
 
     @GetMapping("delete")
     public String deleteEventForm(Model model){
+        model.addAttribute("title", "Delete Events");
         model.addAttribute("events", EventsData.getAll());
         return "events/delete";
     }
@@ -50,7 +53,23 @@ public class EventController {
         return "redirect:";
     }
 
+    @GetMapping("edit/{eventId}")
+    public String displayEditForm(Model model, @PathVariable int eventId){
+        Event eventToEdit = EventsData.getById(eventId);
+        model.addAttribute("event", eventToEdit);
+        String title = "Edit event " + eventToEdit.getName() + "(id=" + eventToEdit.getId() + ")";
+        model.addAttribute("title", title);
+        return "events/edit";
+    }
 
+    @PostMapping("edit")
+    public String processEditForm(int eventId, String name, String description){
+        Event eventToEdit = EventsData.getById(eventId);
+        eventToEdit.setName(name);
+        eventToEdit.setDescription(description);
+        return "redirect:";
+
+    }
 
 
 }
